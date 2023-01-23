d5c6b2707f6a0282738c01200e3f5d34c8a71385
JanineTeodoro <janineteodoro@hotmail.com>
2023-01-23 14:38:50 -0300

    Operadores

--- Java Basico - DIO/operadores/src/Operadores.java
+++ Java Basico - DIO/operadores/src/Operadores.java
@@ -1,4 +1,4 @@
-public class App {
+public class Operadores {
     public static void main(String[] args) throws Exception {
 //        String nomeCompleto = "LINGUAGEM" + "JAVA";
 //        System.out.println(nomeCompleto);

2052754e2d0ae36ca028b4240548f43404f79ed5
JanineTeodoro <janineteodoro@hotmail.com>
2023-01-23 11:07:26 -0300

    Exemplos de tipos e variáveis

---
+++ Java Basico - DIO/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+            if(mediaFinal<6)
+                System.out.println("REPROVADO");
+            else if(mediaFinal==6)
+                System.out.println("PROVA MINERVA");
+                //printIn
+            else
+                System.out.println("APROVADO");
+    }
+}

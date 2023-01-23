386490c93dedb406232b3871e1bfa37fd1408f11
JanineTeodoro <janineteodoro@hotmail.com>
2023-01-23 14:37:14 -0300

    Operadores

---
+++ Java Basico - DIO/operadores/src/App.java
@@ -0,0 +1,85 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+//        String nomeCompleto = "LINGUAGEM" + "JAVA";
+//        System.out.println(nomeCompleto);
+
+
+//        String concatenacao = "?";
+//        concatenacao = 1+1+1+"1";
+//        System.out.println(concatenacao);
+//        concatenacao = 1+"1"+1+1;
+//        System.out.println(concatenacao);
+//        concatenacao = 1+"1"+1+"1";
+//        System.out.println(concatenacao);
+//        concatenacao = "1"+1+1+1;
+//        System.out.println(concatenacao);  
+//        concatenacao = "1"+(1+1+1);
+//        System.out.println(concatenacao);
+
+
+//        int numero = 5;
+//        numero = -numero;
+//        System.out.println(numero);
+//        numero = numero * -1; //forma de tornar o número positivo
+//        System.out.println(numero);
+
+
+//x repetição
+//        int numero = 5;
+//numero++; //mesma coisa que: numero = numero +1;
+//        System.out.println(numero --); 
+//colocando o incremendo ou decremento antes da variável
+//vai imprimir já com o valor incrementado ou decrementado.
+//Se colocar depois vai imprimir somente na próxima impressão.
+//        System.out.println(numero);
+
+
+//        boolean variavel = true;
+//O símbolo ! atribui valor contrário às variáveis booleanas.
+//        variavel = !variavel;
+//        System.out.println(variavel);
+
+
+//ternário (booleano)
+//        int a, b;
+//        a = 6;
+//        b = 6;
+
+//--        String resultado = "";
+//--        if (a==b)
+//--            resultado = "verdadeiro";
+//--        else
+//--            resultado = "falso";
+//Resumindo fica assim:
+//        String resultado = a==b ?"verdadeiro" : "falso";
+//        System.out.println(resultado);
+
+        
+//        String nomeUm = "Janine";
+//        String nomeDois = "Janine";
+//        System.out.println(nomeUm == nomeDois);
+//        int numero1 = 1;
+//        int numero2 = 2;
+//        boolean simNao = numero1 == numero2;
+//        if(numero1 < numero2){
+//            System.out.println("a nossa condição é verdadeira");}
+//Só imprime se for true.
+//        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+//        simNao = numero1 != numero2;
+//        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+//        simNao = numero1 > numero2;
+//        System.out.println("numeroUm é maior a numeroDois? " + simNao);
+
+
+//operadores lógicos
+//        boolean condicao1=true;
+//        boolean condicao2=true;
+//        if (condicao1 && condicao2){
+//            System.out.println("As duas condições são verdadeiras");}
+//        if (condicao1 && (7 > 4)){
+//            System.out.println("As duas condições são verdadeiras");}
+//        if (condicao1 || condicao2){
+//            System.out.println("Uma das condições é verdadeiras");}
+//        System.out.println("fim");
+}
+}

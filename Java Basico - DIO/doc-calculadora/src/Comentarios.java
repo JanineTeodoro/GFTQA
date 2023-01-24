0e2088affe59d92b4f764ea969980675560d2ca6
JanineTeodoro <janineteodoro@hotmail.com>
2023-01-23 21:25:05 -0300

    Java Doc

---
+++ Java Basico - DIO/doc-calculadora/src/Comentarios.java
@@ -0,0 +1,23 @@
+public class Comentarios {
+
+    public static void main(String[] args) {
+    // Olá, eu sou um comentário em uma única linha
+
+    /* Olá,
+    * Eu sou um comentario
+    * que posso ser mais detalhado
+    * quando necessário
+    */
+    }
+
+    /** 
+    * Estas duas estrelinhas acima
+    * é para identificar que você
+    * pretende elaborar um comentário
+    * a nível de documentação.
+    * Que incrível !!!
+    */
+    public void metodo(){
+        
+    }
+}

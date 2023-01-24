d3e732e55fa328a105dd0ab8488f0678d48ded1f
JanineTeodoro <janineteodoro@hotmail.com>
2023-01-24 20:21:48 -0300

    Exercício 04 - Loops

---
+++ Estruturas de Repetição e Arrays em Java/loops/Ex4_ParEImpar/src/Main.java
@@ -0,0 +1,34 @@
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantPares = 0, quantImpares = 0;
+
+        System.out.println("Quantidade de número: ");
+        quantNumeros = scan.nextInt();
+
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if (numero % 2 == 0) quantPares++;
+            else quantImpares++;
+
+            count++;
+        } while (count < quantNumeros);
+
+        System.out.println("Quantidade par: " + quantPares);
+        System.out.println("Quantidade ímpar " + quantImpares);
+    }
+}

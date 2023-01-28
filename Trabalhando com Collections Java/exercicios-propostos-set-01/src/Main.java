45e0dffc150609a261bd262b0f5d431029dd34fa
JanineTeodoro <janineteodoro@hotmail.com>
2023-01-28 10:32:00 -0300

    Exercícios Set

---
+++ Trabalhando com Collections Java/exercicios-propostos-set-01/src/Main.java
@@ -0,0 +1,17 @@
+/*
+Crie uma conjunto contendo as cores do arco-íris e:
+a) Exiba todas as cores o arco-íris uma abaixo da outra;
+b) A quantidade de cores que o arco-íris tem;
+c) Exiba as cores em ordem alfabética;
+d) Exiba as cores na ordem inversa da que foi informada;
+e) Exiba todas as cores que começam com a letra ”v”;
+f) Remova todas as cores que não começam com a letra “v”;
+g) Limpe o conjunto;
+h) Confira se o conjunto está vazio;
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Trabalhando com Collections Java/exercicios-propostos-set-02/src/Main.java
@@ -0,0 +1,16 @@
+/*
+Crie uma classe LinguagemFavorita que possua os atributos nome, anoDeCriacao e IDE.
+Em seguida, crie um conjunto com 3 linguagens e faça um programa que ordene esse conjunto por:
+a) Ordem de inserção;
+b) Ordem natural(nome);
+c) IDE;
+d) Ano de criação e nome;
+e) Nome, ano de criação e IDE;
+Ao final, exiba as linguagens no console, um abaixo da outra.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

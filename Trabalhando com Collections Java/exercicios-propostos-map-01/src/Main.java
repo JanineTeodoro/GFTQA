f18f869c6fe1ccb399f5d3c48d6fb4e6def00b2f
JanineTeodoro <janineteodoro@hotmail.com>
2023-01-28 14:47:09 -0300

    Exercícios Map

---
+++ Trabalhando com Collections Java/exercicios-propostos-map-01/src/Main.java
@@ -0,0 +1,14 @@
+/*
+Dada a população estimada de alguns estados do nordeste brasileiro, faça:
+ Estado = PE - população = 9.616.621
+ Estado = AL - população = 3.351.543
+ Estado = CE - população  = 9.187.103
+ Estado = RN - população = 3.534.265
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        
+    }
+}
---
+++ Trabalhando com Collections Java/exercicios-propostos-map-02/src/Main.java
@@ -0,0 +1,11 @@
+/*
+Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene.
+Depois, mostre quantas vezes cada valor foi conseguido.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        
+    }
+}

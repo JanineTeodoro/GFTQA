8aae852119ca8936df44e6a3168dec9078e97504
JanineTeodoro <janineteodoro@hotmail.com>
2023-01-29 13:01:12 -0300

    final

--- Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio-dio-POO/src/br/com/dio/desafio/dominio/Mentoria.java
+++ Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio-dio-POO/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,28 +2,16 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String decricao;
+public class Mentoria extends Conteudo{
+
     private LocalDate data;
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
     public Mentoria() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDecricao() {
-        return decricao;
-    }
-
-    public void setDecricao(String decricao) {
-        this.decricao = decricao;
     }
 
     public LocalDate getData() {
@@ -37,8 +25,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", decricao='" + decricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
